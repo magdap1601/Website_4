65e9c20cd31efcdc31d95fa2cc3e553911487fd1
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2022-07-17 10:49:14 +0200

    'navigation'

---
+++ main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    let x = document.getElementById("myTopnav");
+    let y = document.getElementById("myTopnav2");
+
+    if (x.className === "navigation__list" && y.className === "navigation__list") {
+        x.className += " responsive";
+        y.className += " responsive";
+    } else {
+        x.className = "navigation__list";
+        y.className = "navigation__list";
+    }
+
+}
